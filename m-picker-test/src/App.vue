ec731f5b1ad85b4b0317afea80555c4af82c1294
liwei <2309485575@qq.com>
2019-05-24 18:36:37 +0800

    省市区组件

---
+++ m-picker-test/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <button @click="toShow">开启</button>
+    <lw-picker
+            :show="show"
+            :link="link"
+            @cancel="close"
+            @confirm="confirmFn"></lw-picker>
+  </div>
+</template>
+
+<script>
+import LwPicker from './components/LwPicker.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      show: false,
+      columns: 3,
+      link:true
+    }
+  },
+  methods:{
+    close() {
+      this.show = false
+    },
+    toShow() {
+      this.show = true
+    },
+    confirmFn(val) {
+      console.log(val,"val");
+      this.show = false
+
+    }
+  },
+  components: {
+    LwPicker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
